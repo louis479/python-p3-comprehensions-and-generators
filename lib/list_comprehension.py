8161c5d894aa5639b9255bed0873441423846a82
louis-ngugi <louisngugi858@gmail.com>
2025-02-17 18:05:22 +0300

    defined and created the num_list and sentence_list

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    return [n for n in num_list if n % 2 == 0]
+    
+print(return_evens([0,1,2,3,4,5,7,8,9,11,13,14,15,17,19,]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
